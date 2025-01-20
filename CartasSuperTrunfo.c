db5fc709548d7ebbb306d3c23f9d1f27792947ee
rhuanlafayette <124814931+rhuanlafayette@users.noreply.github.com>
2025-01-20 22:14:29 +0000

    alteracao do scanf que ocasionava um erro e da variavel double para float

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -26,7 +26,7 @@ int main() {
         populacao1C, populacao2C, populacao3C, populacao4C, populacao1D, populacao2D, populacao3D, populacao4D,
         populacao1E, populacao2E, populacao3E, populacao4E, populacao1F, populacao2F, populacao3F, populacao4F,
         populacao1G, populacao2G, populacao3G, populacao4G, populacao1H, populacao2H, populacao3H, populacao4H;
-        double area1A, area2A, area3A, area4A, area1B, area2B, area3B, area4B, area1C, area2C, area3C, area4C,
+        float area1A, area2A, area3A, area4A, area1B, area2B, area3B, area4B, area1C, area2C, area3C, area4C,
         area1D, area2D, area3D, area4D, area1E, area2E, area3E, area4E, area1F, area2F, area3F, area4F, area1G,
         area2G, area3G, area4G, area1H, area2H, area3H, area4H, PIB1A, PIB2A, PIB3A, PIB4A, PIB1B, PIB2B, PIB3B, PIB4B,
         PIB1C, PIB2C, PIB3C, PIB4C, PIB1D, PIB2D, PIB3D, PIB4D, PIB1E, PIB2E, PIB3E, PIB4E, PIB1F, PIB2F, PIB3F, PIB4F,
@@ -37,29 +37,44 @@ int main() {
         printf("Desafio Cartas Super Trunfo \n");
 
     
-        printf("Para começar o jogo, digite o nome do seu Pais");
-        scanf("%s \n", &pais);
+        printf("Para começar o jogo, digite o nome do seu Pais\n");
+        scanf("%s", &pais);
 
-        printf("Agora, digite o nome do Primeiro Estado: \n");
-        scanf("%s \n", &estadoA);
+        printf("Agora, digite o nome do Primeiro Estado:\n");
+        scanf("%s", &estadoA);
        
         printf("Agora, escolha o nome de 4 cidades para o Primeiro Estado \n");
-        scanf("%s, %s, %s, %s\n", &cidade1A, &cidade2A, &cidade3A, &cidade4A);
+        scanf("%s", &cidade1A);
+        scanf("%s", &cidade2A);
+        scanf("%s", &cidade3A);
+        scanf("%s", &cidade4A);
 
-        printf("Agora informe a Populacao de cada cidade escolhida respectivamente \n");
-        scanf( "%d, %d, %d, %d\n", &populacao1A, &populacao2A, &populacao3A, &populacao4A);
-       
-        printf("Dessa vez, escolha a area de cada cidade respectivamente \n");
-        scanf("%f, %f, %f, %f \n", &area1A, &area2A, &area3A, &area4A);
+        printf("Agora, informe a Populacao de cada cidade escolhida respectivamente \n");
+        scanf("%d", &populacao1A);
+        scanf("%d", &populacao2A);
+        scanf("%d", &populacao3A);
+        scanf("%d", &populacao4A);
 
-        printf("Informe qual sera o PIB de cada cidade respectivamente");
-        scanf("%f, %f, %f, %f \n", &PIB1A, &PIB2A, &PIB3A, &PIB4A);
+        printf("Dessa vez, escolha a area de cada cidade respectivamente (coloque .00) \n");
+        scanf("%f", &area1A);
+        scanf("%f", &area2A);
+        scanf("%f", &area3A);
+        scanf("%f", &area4A);
 
-        printf("Por fim, informe o numero de pontos turisticos em cada cidade respectivamente");
-        scanf("%d, %d, %d, %d \n", &pt1A, &pt2A, &pt3A, &pt4A);
+        printf("Informe qual sera o PIB de cada cidade respectivamente (lembre de colocar .00)\n");
+        scanf("%f", &PIB1A);
+        scanf("%f", &PIB2A);
+        scanf("%f", &PIB3A);
+        scanf("%f", &PIB4A);
+
+        printf("Por fim, informe o numero de pontos turisticos em cada cidade respectivamente\n");
+        scanf("%d", &pt1A);
+        scanf("%d", &pt2A);
+        scanf("%d", &pt3A);
+        scanf("%d", &pt4A);
 
         printf("Chegamos ao fim de suas 4 primeiras cartas referente ao mesmo Estado. Confira:\n");
-        printf("Carta A01: \n Nome do Pais: %s\n Estado: %s\n Cidade: %s\n Populacao: %d\n Area: %f\n, PIB: %f\n Pontos Turisticos: %d\n ", pais, estadoA, cidade1A, populacao1A, area1A, PIB1A, pt1A);
+        printf("Carta A01: \n Nome do Pais: %s\n Estado: %s\n Cidade: %s\n Populacao: %d\n Area: %f\n PIB: %f\n Pontos Turisticos: %d\n ", pais, estadoA, cidade1A, populacao1A, area1A, PIB1A, pt1A);
 
 
         /*printf("Agora, digite o nome do Segundo Estado: \n");
